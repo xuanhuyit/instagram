f6d6f2ec4dff26798e6fd6daf9e3574df8305033
xuanhuyit <32665662+xuanhuyit@users.noreply.github.com>
2018-07-16 10:07:00 +0700

    Add files via upload

---
+++ _next/b608ed7e-a39c-49c1-9590-187c01e175ab/page/get-id.js
@@ -0,0 +1,1 @@
+module.exports=__NEXT_REGISTER_PAGE("/get-id",function(){return{page:webpackJsonp([7],{504:function(e,t,n){e.exports=n(505)},505:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=n(0),r=n.n(a),u=n(22),c=n(28);t.default=function(){return r.a.createElement(u.a,{title:"Get Id"},r.a.createElement(c.a,{type_scan:"get_id"}))}}},[504]).default}});
